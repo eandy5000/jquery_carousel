35ac68a98ca7fd4b2f5376a0726295e31dde73cd
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-26 11:34:37 -0500

    project working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,10 +1,108 @@
 (function(){
     
-array = ["Tom", "Dick", "Harry"];    
+$(document).ready(function(){    
     
-$(document).ready(function(){
-console.log('working');
+// variables    
+var array = ["Tom", "Dick", "Harry"];
+var personArray = [];
+var indexTracker = 0;
+var carouselRunning = false;
+var carouselTimer = 3000;
 
+//functions
+
+function nextIndex () {
+    indexTracker++;
+        if (indexTracker > personArray.length-1) {
+            indexTracker = 0;
+        }
+  
+     return indexTracker;   
+}
+
+function prevIndex () {
+    indexTracker--;
+        if (indexTracker < 0) {
+            indexTracker = personArray.length - 1;
+        }
+     return indexTracker;   
+}
+
+function domInit(){
+     var el = '<img src="'+personArray[indexTracker].imageURL+'">' +
+              '<h1>'+personArray[indexTracker].name +'</h1>' +
+              '<p><a href=" '+personArray[indexTracker].github+' ">'+personArray[indexTracker].github+'</a></p>'+
+              '<p>'+personArray[indexTracker].shoutout+'</p>';
+    
+    $('.out').append(el)
+             .hide()
+             .fadeIn();
+             return;
+}
+
+function domUpdate(){
+   var el = '<img src="'+personArray[indexTracker].imageURL+'">' +
+              '<h1>'+personArray[indexTracker].name +'</h1>' +
+              '<p><a href=" '+personArray[indexTracker].github+' ">'+personArray[indexTracker].github+'</a></p>'+
+              '<p>'+personArray[indexTracker].shoutout+'</p>';
+    
+    $('.out').fadeOut()
+             .empty()
+             .append(el)
+             .hide()
+             .fadeIn();
+             return;
+}
+
+function dataCall (){
+    
+    $.ajax({
+       type: 'GET',
+       url: '/data',
+       success: function(data){
+           personArray = data.zeta;
+           domInit();
+           return;
+       } 
+    });
+    
+}  
+
+function autoCarousel () {
+    setInterval(function(){
+            if (carouselRunning === true) {
+                nextIndex();
+                domUpdate();
+            
+            }
+    }, carouselTimer)
+    
+} 
+    
+
+//jquery program
+dataCall();
+autoCarousel();
+
+//listeners
+$('section').on('click', '.prev', function(){
+   prevIndex();
+   domUpdate();
+});
+
+$('section').on('click', '.next', function(){
+   nextIndex(); 
+   domUpdate();
+});
+
+$('.jumbotron').on('click', '.auto', function(){
+    if (carouselRunning === false) {
+        carouselRunning = true;
+    } else {
+        carouselRunning = false;
+    }
+    console.log(carouselRunning);
+});
 
 
 //end jquery 
